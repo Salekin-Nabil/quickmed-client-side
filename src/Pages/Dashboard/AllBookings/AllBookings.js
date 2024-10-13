5ec63a158c4b9d4e29e2e5deee4cd934cd6da4b6
Abylay Aiyp <ablyaip@gmail.com>
2024-10-12 23:08:57 -0700

    changed some stuff, maybe no need to pull

--- src/Pages/Dashboard/AllBookings/AllBookings.js
+++ src/Pages/Dashboard/AllBookings/AllBookings.js
@@ -1,96 +1,113 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 // import React, { useContext } from 'react';
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../../firebase.init';
-import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { Helmet } from 'react-helmet';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { Helmet } from "react-helmet";
 // import { Link } from 'react-router-dom';
 // import { AuthContext } from '../../../contexts/AuthProvider';
 
 const AllBookings = () => {
-    // const { user } = useContext(AuthContext);
-    const [appointments, setAppoinments] = useState([]);
-    const [user, loading, error] = useAuthState(auth);
-    const navigate = useNavigate();
+  // const { user } = useContext(AuthContext);
+  const [appointments, setAppoinments] = useState([]);
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
 
-    const url = `http://localhost:7000/all_bookings`;
+  const url = `https://quick-med.fly.dev/all_bookings`;
 
-    // const { data: bookings = [] } = useQuery({
-    //     queryKey: ['bookings', user?.email],
-    //     queryFn: async () => {
-    //         const res = await fetch(url, {
-    //             headers: {
-    //                 authorization: `bearer ${localStorage.getItem('accessToken')}`
-    //             }
-    //         });
-    //         const data = await res.json();
-    //         return data;
-    //     }
-    // })
+  // const { data: bookings = [] } = useQuery({
+  //     queryKey: ['bookings', user?.email],
+  //     queryFn: async () => {
+  //         const res = await fetch(url, {
+  //             headers: {
+  //                 authorization: `bearer ${localStorage.getItem('accessToken')}`
+  //             }
+  //         });
+  //         const data = await res.json();
+  //         return data;
+  //     }
+  // })
 
-    useEffect(() => {
-        if(user) {
-            fetch(url, {
-                method: 'GET',
-                headers:{
-                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-            .then(res => {
-                console.log("res:", res);
-                if(res.status === 401 || res.status === 403){
-                    signOut(auth);
-                    localStorage.removeItem("accessToken");
-                    navigate("/");
-                }
-                return res.json()})
-            .then(data => {
-                setAppoinments(data)
-            });
-        }
-    }, [user]);
+  useEffect(() => {
+    if (user) {
+      fetch(url, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => {
+          console.log("res:", res);
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setAppoinments(data);
+        });
+    }
+  }, [user]);
 
-    return (
-        <div className='mb-[60px] mx-[10px]'>
-            <Helmet>
-                <title>QuickMed-All Appointments</title>
-            </Helmet>
-             <h1 className='text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary text-5xl mb-8 font-bold shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray] mx-[1vw] py-[1vw] rounded-lg'>All <span className='text-[#20242c]'>Appointments</span></h1>
-            <div className="overflow-x-auto rounded-xl shadow-xl shadow-[gray]">
-                <table className="table-zebra w-full">
-                    <thead className='bg-gradient-to-br from-accent to-secondary text-white'>
-                        <tr>
-                            <th className='py-5'></th>
-                            <th className='py-5'>Name</th>
-                            <th className='py-5'>Email</th>
-                            <th className='py-5'>Phone Number</th>
-                            <th className='py-5'>Service</th>
-                            <th className='py-5'>Date</th>
-                            <th className='py-5'>Time</th>
-                            <th className='py-5'>Location</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            appointments &&
-                            appointments?.map((appointment, i) => <tr key={appointment._id}>
-                                <th className='text-center py-5'>{i + 1}</th>
-                                <td className='text-sm text-center py-5'>{appointment.username}</td>
-                                <td className='text-sm text-center py-5'>{appointment.email}</td>
-                                <td className='text-sm text-center py-5'>{appointment.phone}</td>
-                                <td className='text-sm text-center py-5'>{appointment.service}</td>
-                                <td className='text-sm text-center py-5'>{appointment.appointmentDate}</td>
-                                <td className='text-sm text-center py-5'>{appointment.slot}</td>
-                                <td className='text-sm text-center py-5'>{appointment.location}</td>
-                            </tr>)
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="mb-[60px] mx-[10px]">
+      <Helmet>
+        <title>QuickMed-All Appointments</title>
+      </Helmet>
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary text-5xl mb-8 font-bold shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray] mx-[1vw] py-[1vw] rounded-lg">
+        All <span className="text-[#20242c]">Appointments</span>
+      </h1>
+      <div className="overflow-x-auto rounded-xl shadow-xl shadow-[gray]">
+        <table className="table-zebra w-full">
+          <thead className="bg-gradient-to-br from-accent to-secondary text-white">
+            <tr>
+              <th className="py-5"></th>
+              <th className="py-5">Name</th>
+              <th className="py-5">Email</th>
+              <th className="py-5">Phone Number</th>
+              <th className="py-5">Service</th>
+              <th className="py-5">Date</th>
+              <th className="py-5">Time</th>
+              <th className="py-5">Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            {!!appointments &&
+              appointments?.map((appointment, i) => (
+                <tr key={appointment._id}>
+                  <th className="text-center py-5">{i + 1}</th>
+                  <td className="text-sm text-center py-5">
+                    {appointment.username}
+                  </td>
+                  <td className="text-sm text-center py-5">
+                    {appointment.email}
+                  </td>
+                  <td className="text-sm text-center py-5">
+                    {appointment.phone}
+                  </td>
+                  <td className="text-sm text-center py-5">
+                    {appointment.service}
+                  </td>
+                  <td className="text-sm text-center py-5">
+                    {appointment.appointmentDate}
+                  </td>
+                  <td className="text-sm text-center py-5">
+                    {appointment.slot}
+                  </td>
+                  <td className="text-sm text-center py-5">
+                    {appointment.location}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllBookings;
--- src/Pages/Dashboard/MyReviews/MyReviews.js
+++ src/Pages/Dashboard/MyReviews/MyReviews.js
@@ -1,63 +1,69 @@
-import React, { useRef, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import Helmet from 'react-helmet';
-import { useForm } from 'react-hook-form';
+import React, { useRef, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Helmet from "react-helmet";
+import { useForm } from "react-hook-form";
 import ReactStars from "react-rating-stars-component";
-import auth from '../../../firebase.init';
-import logo from '../../../assets/icons/logos/quickmed_diagnostic.jpg';
-import toast, { Toaster } from 'react-hot-toast';
-import '../../Shared/gradient.css'
+import auth from "../../../firebase.init";
+import logo from "../../../assets/icons/logos/quickmed_diagnostic.jpg";
+import toast, { Toaster } from "react-hot-toast";
+import "../../Shared/gradient.css";
 
 const MyReviews = () => {
-    const [user] = useAuthState(auth);
-    const {displayName, email} = user;
-    const reviewRef = useRef('');
-    const [review, setReview] = useState(0);
+  const [user] = useAuthState(auth);
+  const { displayName, email } = user;
+  const reviewRef = useRef("");
+  const [review, setReview] = useState(0);
 
-    const ratingChanged = (newRating) => {
-        setReview(newRating);
-      };
+  const ratingChanged = (newRating) => {
+    setReview(newRating);
+  };
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit = () => {
-        const data = {
-            "userName": user.displayName,
-            "userEmail": user.email,
-            "userPhoto": user.photoURL,
-            "ratings": reviewRef.current.value,
-            "reviews": review
-        };
-        console.log(data);
-        
-        const url = `http://localhost:7000/reviews/${email}`;
-        fetch(url, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(res=> res.json())
-        .then(result =>{
-            console.log(result);
-            toast.success('Thanks for your valuable review.');
-        });
+  const { register, handleSubmit } = useForm();
+  const onSubmit = () => {
+    const data = {
+      userName: user.displayName,
+      userEmail: user.email,
+      userPhoto: user.photoURL,
+      ratings: reviewRef.current.value,
+      reviews: review,
     };
-    return (
-        <div>
-            <Helmet>
-                <title>QuickMed-Add a Review</title>
-            </Helmet>
-             <h1 className='text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary text-5xl mb-8 font-bold shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray] mx-[1vw] py-[1vw] rounded-lg'>My <span className='text-[#20242c]'>Review</span></h1>
-             <div className='flex items-center'>
-                <div className='hidden md:block'>
-                    <img src={logo} alt=''/>
-                </div>
-                <div className="block p-6 rounded-xl shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray] bg-white w-full md:mx-10 mb-8">
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <p className='text-2xl text-center font-black mb-4 text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary uppercase'>Add a Review</p>
-                        <div className="form-group mb-6 w-full mr-2">
-                            <input type="text" className="block
+    console.log(data);
+
+    const url = `https://quick-med.fly.dev/reviews/${email}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        toast.success("Thanks for your valuable review.");
+      });
+  };
+  return (
+    <div>
+      <Helmet>
+        <title>QuickMed-Add a Review</title>
+      </Helmet>
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary text-5xl mb-8 font-bold shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray] mx-[1vw] py-[1vw] rounded-lg">
+        My <span className="text-[#20242c]">Review</span>
+      </h1>
+      <div className="flex items-center">
+        <div className="hidden md:block">
+          <img src={logo} alt="" />
+        </div>
+        <div className="block p-6 rounded-xl shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray] bg-white w-full md:mx-10 mb-8">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <p className="text-2xl text-center font-black mb-4 text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary uppercase">
+              Add a Review
+            </p>
+            <div className="form-group mb-6 w-full mr-2">
+              <input
+                type="text"
+                className="block
                                     w-full
                                     px-3
                                     py-1.5
@@ -72,10 +78,15 @@ const MyReviews = () => {
                                     ease-in-out
                                     m-0
                                     focus:text-gray-700 focus:bg-white focus:border-[#194519] focus:outline-none"
-                                    value={displayName} readOnly required/>
-                        </div>
-                        <div className="form-group mb-6 w-full">
-                            <input type="email" className="block
+                value={displayName}
+                readOnly
+                required
+              />
+            </div>
+            <div className="form-group mb-6 w-full">
+              <input
+                type="email"
+                className="block
                                     w-full
                                     px-3
                                     py-1.5
@@ -90,22 +101,26 @@ const MyReviews = () => {
                                     ease-in-out
                                     m-0
                                     focus:text-gray-700 focus:bg-white focus:border-[#194519] focus:outline-none"
-                                    value={email} readOnly required/>
-                        </div>
-                        <div className='flex justify-center mb-4'>
-                            <ReactStars
-                                    count={5}
-                                    onChange={ratingChanged}
-                                    size={24}
-                                    isHalf={true}
-                                    emptyIcon={<i className="far fa-star"></i>}
-                                    halfIcon={<i className="fa fa-star-half-alt"></i>}
-                                    fullIcon={<i className="fa fa-star"></i>}
-                                    activeColor="#ffd700"
-                            />
-                        </div>
-                        <div className="form-group mb-6 w-full mr-2">
-                            <textarea className="form-control block
+                value={email}
+                readOnly
+                required
+              />
+            </div>
+            <div className="flex justify-center mb-4">
+              <ReactStars
+                count={5}
+                onChange={ratingChanged}
+                size={24}
+                isHalf={true}
+                emptyIcon={<i className="far fa-star"></i>}
+                halfIcon={<i className="fa fa-star-half-alt"></i>}
+                fullIcon={<i className="fa fa-star"></i>}
+                activeColor="#ffd700"
+              />
+            </div>
+            <div className="form-group mb-6 w-full mr-2">
+              <textarea
+                className="form-control block
                                     w-full
                                     px-3
                                     py-1.5
@@ -120,17 +135,28 @@ const MyReviews = () => {
                                     ease-in-out
                                     m-0
                                     focus:text-gray-700 focus:bg-white focus:border-[#194519] focus:outline-none"
-                                    ref={reviewRef}
-                                    placeholder="Review Description" required rows={5}/>
-                        </div>
-                                
-                        <div className="form-group form-check text-center mb-6">
-                            <input type="checkbox"
-                                    className="form-check-input appearance-none h-4 w-4 border border-[#194519] rounded-sm bg-white checked:bg-[#194519] checked:border-[#194519] focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
-                            />
-                            <label className="form-check-label inline-block text-[#20242c]" htmlFor="exampleCheck87">Send me a copy of this message</label>
-                            </div>
-                            <button type="submit" className="
+                ref={reviewRef}
+                placeholder="Review Description"
+                required
+                rows={5}
+              />
+            </div>
+
+            <div className="form-group form-check text-center mb-6">
+              <input
+                type="checkbox"
+                className="form-check-input appearance-none h-4 w-4 border border-[#194519] rounded-sm bg-white checked:bg-[#194519] checked:border-[#194519] focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+              />
+              <label
+                className="form-check-label inline-block text-[#20242c]"
+                htmlFor="exampleCheck87"
+              >
+                Send me a copy of this message
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="
                                 w-full
                                 px-6
                                 py-2.5
@@ -147,17 +173,19 @@ const MyReviews = () => {
                                 shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray]
                                 transition
                                 duration-150
-                                ease-in-out">Submit Review</button>
-                    </form>
-                </div>
-                <div className='hidden md:block'>
-                    <img src={logo} alt=''/>
-                    
-                </div>
-            </div>
-            <Toaster/>
+                                ease-in-out"
+            >
+              Submit Review
+            </button>
+          </form>
         </div>
-    );
+        <div className="hidden md:block">
+          <img src={logo} alt="" />
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
 };
 
 export default MyReviews;
