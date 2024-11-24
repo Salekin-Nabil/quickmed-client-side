54fd3aa9d52dc9d553ec078a15a1e1311c117f47
Abylay Aiyp <ablyaip@gmail.com>
2024-11-24 01:00:08 -0800

    feat: human anatomy

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import banner from '../../assets/images/banner.jpg';
-import PrimaryButton from '../Shared/PrimaryButton';
+import { BodyComponent } from '../../components/BodyComponent/BodyComponent';
 
 const Banner = () => {
+    const handlePartClick = (id) => {
+        console.log(`part with id pressed ${id}`);
+    };
     return (
-        <div className='mb-12'>
-            <div className="carousel-inner relative w-full overflow-hidden rounded-xl shadow-lg shadow-[gray]">
-                <div className="carousel-item active relative float-left w-full">
-                    <img
-                        src={banner}
-                        className="block w-full md:h-[41vw]"
-                        alt="..."
-                    />
-                </div>
+        <div className='mb-12 flex flex-col'>
+            <BodyComponent
+                onClick={handlePartClick}
+                partsInput={{
+                    head: { show: true },
+                    leftShoulder: { show: true },
+                    rightShoulder: { show: true },
+                    leftArm: { show: true },
+                    rightArm: { show: true },
+                    chest: { show: true },
+                    stomach: { show: true },
+                    leftLeg: { show: true },
+                    rightLeg: { show: true },
+                    leftHand: { show: true },
+                    rightHand: { show: true },
+                    leftFoot: { show: true },
+                    rightFoot: { show: true },
+                }}
+                mode={"default"}
+            />
+            <div className='flex items-center justify-center'>
+                <h2 className='text-lg font-semibold text-gray-800 text-center'>
+                    Pick the area of your body experiencing discomfort.
+                </h2>
             </div>
         </div>
     );
